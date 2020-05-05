e50018f4c3381d034a5b66fcb689842f2a3c6bda
JambonoLeJambon <60428074+JambonoLeJambon@users.noreply.github.com>
2020-05-05 18:57:52 +0200

    visuel php

---
+++ MonEspace.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Projet PHP</title>
+        <link rel="stylesheet" type="text/css" href="index.css">
+        <meta name="viewport" content="width:device-width, initial-scale=1.0">
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <!--Haut de la page-->
+        <div class="header">
+          <h1>Mon espace</h1>
+        </div>
+        <!--Fin du haut de la page-->
+        <!--Barre de navigation-->
+        <div class="navbar">
+            <ul class="navbar">
+                    <li><a href="MonEspace.php" class="active">Accueil</a></li>
+                    <li class="dropdown">
+                        <a href="javascript:void(0)" class="dropbtn">Informations</a>
+                            <div class="dropdown-content">
+                                <a href="Chateau.html">Profil</a>
+                                <a href="Visite.html">Déconnexion</a>
+                            </div>
+                    </li>
+                    <li><a href="#">Autres...</a></li>
+            </ul>
+        </div>
+        <!--Fin de la barre de navigation-->
+    </body>
+</html>
